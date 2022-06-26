cc96f2dd6d94c0cefe892dc539cd910b314be2d8
Spud97 <luca.dercole97@gmail.com>
2022-06-26 22:59:58 +0200

    bloccato

---
+++ src/components/SearchDisc.vue
@@ -0,0 +1,41 @@
+<template>
+  <form class="d-flex">
+    <div class="input-group">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Genere musicale"
+        v-model="searchText"
+      />
+      <button class="btn btn-outline-primary" type="button" @click="makeSearch">
+        Cerca
+      </button>
+      <button class="btn btn-outline-secondary" type="reset" @click="makeReset">
+        Reset
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    genere: String,
+  },
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+  methods: {
+    makeSearch() {
+      this.$emit("search", this.searchText);
+    },
+    makeReset() {
+      this.$emit("reset");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,56 @@
 <template>
   <div class="bg-header">
-    <div class="container">
-      <i class="fa-brands fa-spotify spotify-icon my-2"></i>
+    <div class="container d-flex justify-content-between">
+      <div>
+        <i class="fa-brands fa-spotify spotify-icon my-2"></i>
+      </div>
+      <div class="d-flex align-items-center">
+        <SearchDisc>
+          @search="onSearchChange" @reset="onSearchReset" :genere="genere"
+        </SearchDisc>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import discList from "./TheMain.vue";
+import SearchDisc from "./SearchDisc.vue";
 export default {
   name: "TheHeader",
+  components: { SearchDisc },
+  props: {
+    genere: String,
+  },
+  data() {
+    return {
+      discList,
+      searchText: ""
+    }
+  },
+  computed: {
+    getGenresList() {
+      const list = [];
+      this.discList.forEach((disc) => {
+        if (!list.includes(disc.genre)) {
+          list.push(disc.genre);
+        }
+      });
+      return list;
+    },
+  },
+  methods: {
+    onSearchChange() {
+      return this.discList.filter((disc) => {
+        return disc.genre
+          .toLowerCase()
+          .includes(this.searchText.trim(" ").toLowerCase());
+      });
+    },
+    onSearchReset() {
+      this.searchText = ""
+    },
+  },
 };
 </script>
 
